7e5cc57f310fa072f768c2448128b71a360e5d6d
Daniel Salazar <dsalazar10@horizon.csueastbay.edu>
2018-02-11 18:45:19 -0800

    Update appIconMaker.py

--- appIconMaker.py
+++ appIconMaker.py
@@ -9,6 +9,7 @@ def mkdir(folder):
   except OSError:
     pass
   os.chdir(folder)
+  
 
 # image file names
 filenames = ["iphone-20@2x.png", "iphone-20@3x.png", "iphone-29@2x.png",
@@ -18,8 +19,8 @@ filenames = ["iphone-20@2x.png", "iphone-20@3x.png", "iphone-29@2x.png",
              "ipad-40@1x.png", "ipad-40@2x.png", "ipad-76@1x.png",
              "ipad-76@2x.png", "ipad-83.5@2x.png", "ios-marketing-1024@1x.png"]
 # image sizes            
-size = [20, 20, 29, 29, 40, 40, 60, 60, 20, 20, 29, 29, 40, 40, 76, 76,83.5,
-         1024]
+size = [40, 60, 58, 87, 80, 120, 120, 180, 20, 40, 29, 58, 40, 80, 76, 152,
+        167, 1024]
 
 # load image into openCV
 img = cv2.imread('Logo.png')
